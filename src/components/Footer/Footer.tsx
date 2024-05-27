8b6f73420c2252717da102601362764d24621626
surpri6e <dan.surpri6e@gmail.com>
2024-05-27 10:42:58 +0300

    not working

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,18 +1,34 @@
 import './Footer.scss';
 
 const Footer = () => {
+    const onButtonClick = (name: string) => {
+        // using Java Script method to get PDF file
+        fetch(`../../../public/${name}`).then((response) => {
+            response.blob().then((blob) => {
+                // Creating new object of PDF file
+                const fileURL = window.URL.createObjectURL(blob);
+
+                // Setting various property values
+                const alink = document.createElement('a');
+                alink.href = fileURL;
+                alink.download = name;
+                alink.click();
+            });
+        });
+    };
+
     return (
         <footer className='footer'>
             <div className='_Container'>
                 <nav className='footer_nav'>
                     <ul className='footer_list'>
                         <li className='footer_elem'>
-                            <a href='../../../public/Surphoto1.0.0.exe' className='footer_link' target='_blank'>
+                            <a onClick={() => onButtonClick('Surphoto1.0.0.exe')} className='footer_link'>
                                 Скачать
                             </a>
                         </li>
                         <li className='footer_elem'>
-                            <a href='../../../public/policy.docx' className='footer_link' target='_blank'>
+                            <a onClick={() => onButtonClick('policy.docx')} className='footer_link'>
                                 Политика конфедициальности
                             </a>
                         </li>
